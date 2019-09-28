62873d1fd4fa0caddb42251f2a57b71a6a2e0f10
永田大祐 <dbank0208@gmail.com>
2019-09-28 23:07:21 +0900

    change logic

--- MVVMArchitecture/ViewController.swift
+++ MVVMArchitecture/ViewController.swift
@@ -17,18 +17,10 @@ class ViewController: UIViewController {
 
         views.bt.addTarget(self, action: #selector(btAction), for: .touchUpInside)
         view.addSubview(views)
-        views.vm.observe(for: views.vm.model) {
-            [weak self ](value) in
-            guard let selfStrong = self else { return }
-            selfStrong.views.labelOne.text = value.one
-            selfStrong.views.labelTwo.text = value.two
-        }
     }
 
     @objc func btAction() {
-        guard views.labelOne.text == "123" else {
-            return views.vm.valueSet(Model.init(one: "123", two: "456"))
-        }
+        guard views.vm.model.value?.one == "123" else { return views.vm.valueSet(Model.init(one: "123", two: "456")) }
         views.vm.valueSet(Model.init(one: "456", two: "123"))
     }
 }
