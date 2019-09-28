41f997ae26496cd49bfaeb6ba4184de0ee5aa78c
永田大祐 <dbank0208@gmail.com>
2019-09-29 08:35:26 +0900

    add autolayaut

--- MVVMArchitecture/View/DesinView.swift
+++ MVVMArchitecture/View/DesinView.swift
@@ -9,13 +9,16 @@
 import UIKit
 
 final class DesinView: UIView {
-    
+
     let vm = ViewModel()
     let labelOne = UILabel()
     let imageOne = UIImageView()
     let labelTwo = UILabel()
     let imageTwo = UIImageView()
     let bt = UIButton()
+
+    private let topHeight: CGFloat = UINavigationController.init().navigationBar.frame.height +
+    (UIWindow.init().windowScene?.statusBarManager?.statusBarFrame.height ?? UIApplication.shared.statusBarFrame.height)
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -29,18 +32,37 @@ final class DesinView: UIView {
 
     private func desgin() {
         self.frame = UIScreen.main.bounds
-        labelOne.frame = CGRect(x: 0, y: 100, width: self.frame.width, height: 100)
-        imageOne.frame = CGRect(x: self.frame.width-100, y: 100, width: 100, height: 100)
-        labelTwo.frame = CGRect(x: 0, y: 200, width: self.frame.width, height: 100)
-        imageTwo.frame = CGRect(x: self.frame.width-100, y: 200, width: 100, height: 100)
-        bt.frame = CGRect(x: 0, y: 300, width: self.frame.width, height: 100)
-
-        bt.backgroundColor = .yellow
 
         self.addSubview(labelOne)
         self.addSubview(imageOne)
         self.addSubview(labelTwo)
         self.addSubview(imageTwo)
         self.addSubview(bt)
+        
+        layoutAnchor(labelOne, xAxisAnchor: self.centerXAnchor, constantY: self.topAnchor, widthmultiplier: 1, heightmultiplier: 0.1, height: topHeight)
+        layoutAnchor(imageOne, xAxisAnchor: labelOne.centerXAnchor, constantX: self.frame.width/2 - self.frame.width/9.96, constantY: self.topAnchor, widthmultiplier: 0.2, heightmultiplier: 0.1, height: topHeight)
+
+        layoutAnchor(labelTwo, xAxisAnchor: self.centerXAnchor, constantY: labelOne.bottomAnchor, widthmultiplier: 1, heightmultiplier: 0.1)
+        layoutAnchor(imageTwo, xAxisAnchor: imageOne.centerXAnchor, constantY: labelOne.bottomAnchor, widthmultiplier: 0.2, heightmultiplier: 0.1)
+
+        layoutAnchor(bt, xAxisAnchor: self.centerXAnchor, constantY: labelTwo.bottomAnchor, widthmultiplier: 1, heightmultiplier: 0.1)
+
+        labelOne.backgroundColor = .red
+        labelTwo.backgroundColor = .blue
+        bt.backgroundColor = .yellow
     }
+
+    private func layoutAnchor(_ v             : UIView,
+                              xAxisAnchor     : NSLayoutXAxisAnchor,
+                              constantX       : CGFloat? = nil,
+                              constantY       : NSLayoutYAxisAnchor,
+                              widthmultiplier : CGFloat? = nil,
+                              heightmultiplier: CGFloat? = nil,
+                              height          : CGFloat? = nil) {
+             v.translatesAutoresizingMaskIntoConstraints = false
+             v.centerXAnchor.constraint(equalTo: xAxisAnchor, constant: constantX ?? 0).isActive = true
+             v.topAnchor.constraint(equalTo    : constantY, constant: height ?? 0).isActive = true
+             v.widthAnchor.constraint(equalTo  : self.widthAnchor, multiplier: widthmultiplier ?? 0).isActive = true
+             v.heightAnchor.constraint(equalTo : self.heightAnchor, multiplier: heightmultiplier ?? 0).isActive = true
+         }
 }
