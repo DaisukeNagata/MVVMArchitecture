8fccf78c56f5769ef8f2a25d4951972c45fa1c6e
永田大祐 <dbank0208@gmail.com>
2019-10-03 22:27:31 +0900

    clear intent

--- MVVMArchitecture/Model/APIModel.swift
+++ MVVMArchitecture/Model/APIModel.swift
@@ -11,28 +11,28 @@ import Foundation
 class APIModel: NetWorkAndBind {
 
     func networkIsReady<M>(_ url: URL,completion: @escaping (_ model: M?) -> Void) {
-           // example view set
-           request(from: url, { (_ result: Result<M, Error>) in
-               switch result {
-               case .success(let s):
-                   completion(s)
-               case.failure(let f):
-                   print(f)
-               }
-           })
-       }
+        // example view set
+        request(from: url, { (_ result: Result<M, Error>) in
+            switch result {
+            case .success(let s):
+                completion(s)
+            case.failure(let f):
+                print(f)
+            }
+        })
+    }
 
-       func request<J>(from url: URL, _ completion: @escaping (Result<J, Error>) -> Void) {
-           ApiClient.request(url: url, httpMethod: "GET", completion:{ data, res, error in
-               do{
-                   if let data = data {
-                       let decoder: JSONDecoder = JSONDecoder()
-                       guard let newJson = try decoder.decode(Model.self, from: data) as? J else { return }
-                       completion(.success(newJson))
-                   }
-               } catch{
-                   print("Serialize Error.")
-               }
-           })
-       }
+    func request<J>(from url: URL, _ completion: @escaping (Result<J, Error>) -> Void) {
+        ApiClient.request(url: url, httpMethod: "GET", completion:{ data, res, error in
+            do{
+                if let data = data {
+                    let decoder: JSONDecoder = JSONDecoder()
+                    guard let newJson = try decoder.decode(Model.self, from: data) as? J else { return }
+                    completion(.success(newJson))
+                }
+            } catch{
+                print("Serialize Error.")
+            }
+        })
+    }
 }
