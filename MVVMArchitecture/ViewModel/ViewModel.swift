4e75f63d15149ced64caa2358b82d1e81b32f239
daisukenagata <dbank0208@gmail.com>
2019-10-09 08:14:32 +0900

    data judge

--- MVVMArchitecture/ViewModel/ViewModel.swift
+++ MVVMArchitecture/ViewModel/ViewModel.swift
@@ -13,7 +13,8 @@ class ViewModel: Observer, APIConnect {
     var api            : APIModel? = nil
     var model          : Observable<Model>?
     var modelPrimitive : Observable<ModelPrimitive>?
-
+    var valueData      : Data?
+    var oldData        : Data?
     init() {
         api = APIModel()
         model = Observable()
@@ -24,7 +25,14 @@ class ViewModel: Observer, APIConnect {
 
     func valueSet(_ model: Any) { self.model?.value = model as? Model }
     
-    func titleSet(_ model: Any) { self.modelPrimitive?.value = model as? ModelPrimitive }
+    func titleSet(_ model: ModelPrimitive) {
+        valueData = (model as AnyObject).debugDescription.data(using: .utf8)
+        guard valueData == oldData else {
+            oldData = valueData
+            self.modelPrimitive?.value = model
+            return
+        }
+    }
 
     func observe<O>(for observable: Observable<O>, with: @escaping (O) -> Void) { observable.bind(observer: with) }
 }
